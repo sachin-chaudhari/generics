52162a0db4e4152cfa7cedc31b6376e32c60c1ab
sachin-chaudhari <sssachrocks@gmail.com>
2017-04-16 23:25:25 +0530

    Sort

--- Generics/src/com/sachin/sort/Sort.java
+++ Generics/src/com/sachin/sort/Sort.java
@@ -1,5 +1,19 @@
 package com.sachin.sort;
 
 public class Sort {
+	public static <T extends Comparable<T>> void bubbleSort(T[] array) {
+		for(int i = 0; i < array.length; i++)
+		{
+			for(int j = 0; j < array.length - i; j++)
+			{
+				if(array[i].compareTo(array[j]) > 0)
+				{
+					T temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+	}
 
 }
